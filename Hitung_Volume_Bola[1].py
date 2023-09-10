a0d772c057a45737d362122b3e3536c04c177769
jamilz123456 <144548034+jamilz123456@users.noreply.github.com>
2023-09-10 08:48:56 +0700

    Add files via upload

---
+++ Hitung_Volume_Bola[1].py
@@ -0,0 +1,11 @@
+# Program Menghitung Volume Bola
+
+print("Program Menghitung Volume Bola")
+
+pi = 3.14
+r = int(input("Masukkan Jari-jari Bola : "))
+
+
+V = 4/3 * pi * r ** 3
+print("Rumus Menghitung Volume Bola : V = 4/3 x pi x r**3")
+print("Hasil dari Hitung Volume Bola : ",V)
